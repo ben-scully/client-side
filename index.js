c7c09c9c7a580a5f928b97626fea130c72662020
xxPicasso <ben@moregolf.co.nz>
2016-04-04 10:12:19 +1200

    release 0, 1  & 3 complete. Skipped release 2

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
-  if (err) console.log(err) // do something
-
+xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
+  if (err) {
+    throw err
+  }
+  // For Testing
   console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+  var content = data.body
 })
 
+
+document.getElementById("wizardBtn").addEventListener("click", function(){
+
+    xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
+      if (err) {
+        throw err
+      }
+      // For Testing
+      console.log(data.body)
+
+      var content = data.body
+      document.getElementById("wizardBox").innerHTML = content
+    })
+})
