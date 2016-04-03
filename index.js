dde1613346bca7c7322e8e983d384ef452f28a45
xxPicasso <ben@moregolf.co.nz>
2016-04-04 10:35:30 +1200

    my release0/1/3 were actually not using handle bars. now they are.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
-  if (err) {
-    throw err
-  }
-  // For Testing
-  console.log(data.body)
-  var content = data.body
-})
-
 
 document.getElementById("wizardBtn").addEventListener("click", function(){
 
-    xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
+    // 25544
+    xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
       if (err) {
         throw err
       }
       // For Testing
-      console.log(data.body)
+      console.log("Raw: ", data.body)
 
-      var content = data.body
+      var rawToObj = JSON.parse(data.body)
+      console.log("Raw To Obj: ", rawToObj)
+
+      var partObj = rawToObj[0]
+      console.log("Part Obj: ", partObj)
+
+      console.log("Gets Name?: ", partObj.name)
+
+      var content = example( partObj )
       document.getElementById("wizardBox").innerHTML = content
     })
 })
