9f6a5c2351569b4c997aa278170847bbb543b9f2
xxPicasso <ben@moregolf.co.nz>
2016-04-04 10:24:46 +1200

    Updates

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
-  if (err) {
-    throw err
-  }
-  // For Testing
-  console.log(data.body)
-  var content = data.body
-})
-
 
 document.getElementById("wizardBtn").addEventListener("click", function(){
 
-    xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
+    // 25544
+    xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
       if (err) {
         throw err
       }
       // For Testing
-      console.log(data.body)
+      // console.log(data.body)
 
-      var content = data.body
-      document.getElementById("wizardBox").innerHTML = example( content )
+      var content = example( data.body )
+      document.getElementById("wizardBox").innerHTML = content
     })
 })
