440deed4acc12f9c399089c82fe70f2914c37117
xxPicasso <ben@moregolf.co.nz>
2016-04-04 12:05:34 +1200

    got jade & jadeify working

--- index.js
+++ index.js
@@ -5,23 +5,26 @@ var example = require('./views/jadeExample.jade')
 
 document.getElementById("wizardBtn").addEventListener("click", function(){
 
-    // 25544
-    xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
-      if (err) {
-        throw err
-      }
-      // For Testing
-      console.log("Raw: ", data.body)
+  console.log("Button Pressed --- ")
 
-      var rawToObj = JSON.parse(data.body)
-      console.log("Raw To Obj: ", rawToObj)
+  xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
+    if (err) {
+      throw err
+    }
+    // For Testing
+    console.log("Raw: ", data.body)
 
-      var partObj = rawToObj[0]
-      console.log("Part Obj: ", partObj)
+    var rawToObj = JSON.parse(data.body)
+    console.log("Raw To Obj: ", rawToObj)
 
-      console.log("Gets Name?: ", partObj.name)
+    console.log("NEW Name Test: ", rawToObj.name)
 
-      var content = example( partObj )
-      document.getElementById("wizardBox").innerHTML = content
-    })
+    // var partObj = rawToObj[0]
+    // console.log("Part Obj: ", partObj)
+
+    // console.log("Gets Name?: ", partObj.name)
+
+    var content = example( rawToObj )
+    document.getElementById("wizardBox").innerHTML = content
+  })
 })
